b3024d4ff88e5c3ca2a68ffeb655894fb2eb0cf3
Fig Yogurt <itisaftab@gmail.com>
2025-10-28 04:59:38 -0400

    feat(notes): :memo: created modul for notes

---
+++ app/modules/notes/model.js
@@ -0,0 +1,25 @@
+const { Model } = require('app/modules/common')
+
+class NoteModel extends Model {
+  schema() {
+    return {
+      title: {
+        type: String,
+        trim: true,
+        required: true
+      },
+      message: {
+        type: String,
+        trim: true,
+        required: true
+      },
+      userId: {
+        type: String,
+        ref: 'User',
+        required: true
+      }
+    }
+  }
+}
+
+module.exports = NoteModel
