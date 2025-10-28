e49a8f91d7a8c079aafab8c8e8cac3ed688be1dc
Fig Yogurt <itisaftab@gmail.com>
2025-10-28 04:35:03 -0400

    fix(status): :wrench: updated 'Get /dev/status' to correctly show when server is connected to database

--- app/api/status/controller.js
+++ app/api/status/controller.js
@@ -1,5 +1,20 @@
+const mongoose = require('mongoose')
+
+const status = {
+  0: 'disconnected',
+  1: 'connected',
+  2: 'connecting',
+  3: 'disconnecting'
+}
+
 exports.currentStatus = function (req, res) {
-  res.status(200).send({
-    status: 'OK'
-  })
+  const { readyState } = mongoose.connection
+
+  if (readyState === 1) {
+    return res.status(200).send({
+      status: 'OK'
+    })
+  } else {
+    res.status(500).send({ error: `Database connection is ${status[readyState]}! It is not connected!` })
+  }
 }
